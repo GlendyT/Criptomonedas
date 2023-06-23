ec6ee74e1f0ff42cedf0e1b21ed1fe54ef586855
Glendy <glendytuyuc@gmail.com>
2023-06-23 17:09:25 -0600

    first commit

---
+++ src/Data/monedas.js
@@ -0,0 +1,12 @@
+
+const monedas = [
+    {id: "USD", nombre: "Dolar de Estados Unidos"},
+    {id: "MXN", nombre: "Peso Mexicano"},
+    {id: "EUR", nombre: "Euros"},
+    {id: "GBP", nombre: "Libra Esterlina"},
+    {id: "KRW", nombre: "Won Surcoreano"},
+]
+
+export {
+    monedas 
+}
